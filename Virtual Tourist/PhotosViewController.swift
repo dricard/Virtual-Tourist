be3c189984a780e524f45f1b9bdb52fdece684ca
Denis Ricard <d.ricard@me.com>
2016-10-25 11:22:29 -0400

    add: PhotosViewController

---
+++ Virtual Tourist/PhotosViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotosViewController.swift
+//  Virtual Tourist
+//
+//  Created by Denis Ricard on 2016-10-23.
+//  Copyright © 2016 Hexaedre. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class PhotosViewController: UIViewController {
+   
+   // MARK: - properties
+   var pin: Pin?
+   var managedContext: NSManagedObjectContext!
+   var focusedRegion: MKCoordinateRegion?
+   
+   // MARK: - Outlets
+
+   @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+   @IBOutlet weak var collectionView: UICollectionView!
+   @IBOutlet weak var mapView: MKMapView!
+   
+   // MARK: - Life cycle
+   
+   override func viewDidLoad() {
+      
+      super.viewDidLoad()
+      
+   }
+   
+}
