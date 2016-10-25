c54fee0b2cc57751b1041a16d05e166a47d341b2
Denis Ricard <d.ricard@me.com>
2016-10-25 11:49:15 -0400

    feat: set focused region on map portion of PhotosViewController, add CollectionView DataSource methods

--- Virtual Tourist/PhotosViewController.swift
+++ Virtual Tourist/PhotosViewController.swift
@@ -18,7 +18,7 @@ class PhotosViewController: UIViewController {
    var focusedRegion: MKCoordinateRegion?
    
    // MARK: - Outlets
-
+   
    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
    @IBOutlet weak var collectionView: UICollectionView!
    @IBOutlet weak var mapView: MKMapView!
@@ -26,9 +26,77 @@ class PhotosViewController: UIViewController {
    // MARK: - Life cycle
    
    override func viewDidLoad() {
-      
       super.viewDidLoad()
+      
+      // set up portion of the map with the selected pin
+      mapView.setRegion(focusedRegion!, animated: true)
+      // Drop a pin at that location
+      
+      let lat = focusedRegion!.center.latitude
+      let lon = focusedRegion!.center.longitude
+      
+      let locationCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+      
+      let annotation = MKPointAnnotation()
+      annotation.coordinate = locationCoordinate
+      annotation.title = "Photos from here"
+      
+      mapView.addAnnotation(annotation)
+      
+      // set delegates for the collectionView
+      
+      collectionView.delegate = self
+      collectionView.dataSource = self
+      
+      
+   }
+}
+
+extension PhotosViewController: UICollectionViewDelegate {
+   
+}
+
+extension PhotosViewController: UICollectionViewDataSource {
+   
+   // MARK: - CollectionViewController subclass required methods
+   
+   func numberOfSections(in collectionView: UICollectionView) -> Int {
+      //      let sections = self.frc?.sections?.count ?? 0
+      //      return sections
+      return 1
+   }
+   
+   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      
+      guard let photos = pin?.photos else { return 1 }
+      
+      return photos.count
       
    }
    
+   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      
+      // Create a cell
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+      
+      guard let photo = pin?.photos?[indexPath.item] as Photo else { return cell }
+      
+      // Configure the cell
+      
+      if let imagePath = photo.imagePath {
+         let imageURL = URL(string: imagePath)
+         if let imageData = try? Data(contentsOf: imageURL!) {
+            cell.imageView.image = UIImage(data: imageData)
+         } else {
+            print("Unable to get imageData from imageURL")
+         }
+      } else {
+         cell.imageView.image = UIImage(named: "cube")
+      }
+      
+      return cell
+   }
+   
 }
+
+
