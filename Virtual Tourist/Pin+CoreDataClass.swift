ef7a5338781b3f0cbb1b2bd4b2e5c6cce98e5812
Denis Ricard <d.ricard@me.com>
2016-10-25 11:24:46 -0400

    add: Pin+CoreDataClass

---
+++ Virtual Tourist/Pin+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Pin+CoreDataClass.swift
+//  Virtual Tourist
+//
+//  Created by Denis Ricard on 2016-10-22.
+//  Copyright © 2016 Hexaedre. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Pin: NSManagedObject {
+
+}
