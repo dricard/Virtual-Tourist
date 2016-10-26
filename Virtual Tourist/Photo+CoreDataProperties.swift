0f2f38ca870134584c12658682f8750ee037b28c
Denis Ricard <d.ricard@me.com>
2016-10-26 15:40:44 -0400

    feat: add id property to Photo model (to sort by)

--- Virtual Tourist/Photo+CoreDataProperties.swift
+++ Virtual Tourist/Photo+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Photo+CoreDataProperties.swift
 //  Virtual Tourist
 //
-//  Created by Denis Ricard on 2016-10-22.
+//  Created by Denis Ricard on 2016-10-26.
 //  Copyright © 2016 Hexaedre. All rights reserved.
 //
 
@@ -15,8 +15,9 @@ extension Photo {
         return NSFetchRequest<Photo>(entityName: "Photo");
     }
 
-    @NSManaged public var title: String?
     @NSManaged public var imageURL: String?
+    @NSManaged public var title: String?
+    @NSManaged public var id: String?
     @NSManaged public var pin: Pin?
 
 }
